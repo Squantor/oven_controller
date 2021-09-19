9686303cac9358cb9eb644fd3a13182694ebe142
Squantor <squantmuts@gmail.com>
2021-09-19 14:22:36 +0200

    added some pins to the oven controller

--- software/oven_controller/inc/proto_board.hpp
+++ software/oven_controller/inc/proto_board.hpp
@@ -32,4 +32,36 @@
 #define UART_DEBUG          USART0
 #define UART_BAUD_RATE      (115200u)
 
+// Bunch of test pins
+#define IOCON_TESTPIN0      (IOCON_PIO1_10)
+#define PORT_TESTPIN0       (GPIO_PORT1)
+#define PIN_TESTPIN0        (10u)
+#define SWM_TESTPIN0        (SWM_PORTPIN_P1_10)
+
+#define IOCON_TESTPIN1      (IOCON_PIO1_10)
+#define PORT_TESTPIN1       (GPIO_PORT1)
+#define PIN_TESTPIN1        (11u)
+#define SWM_TESTPIN1        (SWM_PORTPIN_P1_11)
+
+#define IOCON_TESTPIN2      (IOCON_PIO0_18)
+#define PORT_TESTPIN2       (GPIO_PORT0)
+#define PIN_TESTPIN2        (18u)
+#define SWM_TESTPIN2        (SWM_PORTPIN_P0_18)
+
+#define IOCON_TESTPIN3      (IOCON_PIO1_21)
+#define PORT_TESTPIN3       (GPIO_PORT1)
+#define PIN_TESTPIN3        (21u)
+#define SWM_TESTPIN3        (SWM_PORTPIN_P1_21)
+
+#define IOCON_TESTPIN4      (IOCON_PIO1_20)
+#define PORT_TESTPIN4       (GPIO_PORT1)
+#define PIN_TESTPIN4        (20u)
+#define SWM_TESTPIN4        (SWM_PORTPIN_P1_20)
+
+// Zero cross detector input pin
+#define IOCON_ZEROCROSS     (IOCON_PIO0_29)
+#define PORT_ZEROCROSS      (GPIO_PORT0)
+#define PIN_ZEROCROSS       (29u)
+#define SWM_ZEROCROSS       (SWM_PORTPIN_P0_29)
+
 #endif
