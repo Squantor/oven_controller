761d4e4ab3992b22c82dcf8f6942e3c1e835730c
Squantor <squantmuts@gmail.com>
2021-10-22 21:57:44 +0200

    50Hz PWM output works, feeding uart chars changes the PWM output

--- software/oven_controller/src/main.cpp
+++ software/oven_controller/src/main.cpp
@@ -21,10 +21,17 @@ int main()
     char character;
     boardInit();
     dsPuts(&streamUart, "Hello World!\n");
+    uint16_t pwm_value = 0;
     while (1) 
     {
         result readState = dsReadChar(&streamUart, &character);
         if(readState == noError)
+        {
             dsWriteChar(&streamUart, character);
+            SctMatchReloadH(SCT0, SCT_MATCH_1, pwm_value);
+            pwm_value+=200;
+            if(pwm_value > 5000)
+                pwm_value = 0;
+        }
     }
 }
