6137674dec8b9c74423a35cbdf6b7c5f2976d5ac
Squantor <squantmuts@gmail.com>
2021-11-12 10:56:11 +0100

    implemented per phase control scheme

--- software/oven_controller/src/proto_board.cpp
+++ software/oven_controller/src/proto_board.cpp
@@ -32,8 +32,8 @@ void setup50HzOutput(void)
         SCT_CTRL_CLRCTR_L );
     SctMatchH(SCT0, SCT_MATCH_0, 5000);
     SctMatchReloadH(SCT0, SCT_MATCH_0, 5000);
-    SctMatchH(SCT0, SCT_MATCH_1, 4950);
-    SctMatchReloadH(SCT0, SCT_MATCH_1, 4950);
+    SctMatchH(SCT0, SCT_MATCH_1, 4900);
+    SctMatchReloadH(SCT0, SCT_MATCH_1, 4900);
     SctSetEventStateMask(SCT0, SCT_EVENT_0_VAL, SCT_STATE_ALL_BIT);
     SctSetEventControl(SCT0, SCT_EVENT_0_VAL, 
         SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH) | 
@@ -56,9 +56,11 @@ void boardInit(void)
     ioconSetupPin(IOCON, IOCON_UART_TX, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_TESTPIN0, IOCON_MODE_INACTIVE);
     ioconSetupPin(IOCON, IOCON_ZEROCROSS, IOCON_MODE_INACTIVE);
+    ioconSetupPin(IOCON, IOCON_TRIAC, IOCON_MODE_INACTIVE);
     SwmMovablePinAssign(SWM, SWM_USART0_TXD, SWM_UART_TX);
     SwmMovablePinAssign(SWM, SWM_USART0_RXD, SWM_UART_RX);
     SwmMovablePinAssign(SWM, SWM_SCT_OUT0, SWM_TESTPIN0);
+    SwmMovablePinAssign(SWM, SWM_SCT_OUT1, SWM_TRIAC);
     SwmMovablePinAssign(SWM, SWM_SCT_PIN0, SWM_ZEROCROSS);
     inputMuxSct(INPUTMUX, SCT_INMUX_INPUT_0, SCT_MUX_PIN0);
     // setup crystal oscillator to run core at 12MHz
@@ -92,37 +94,51 @@ void boardInit(void)
     // setup SCT
     SctConfig(SCT0, SCT_CONFIG_16BIT_COUNTER | SCT_CONFIG_AUTOLIMIT_H);
 
-    // if match 2 is reached, disable output and stop and reset the timer
-    SctMatchL(SCT0, SCT_MATCH_2, 6000);
-    SctMatchReloadL(SCT0, SCT_MATCH_2, 6000);
+    // if match 2 is reached, disable output and stop the timer
+    SctMatchL(SCT0, SCT_MATCH_2, 11000);
+    SctMatchReloadL(SCT0, SCT_MATCH_2, 11000);
     // assign event 2 to match 2
     SctSetEventControl(SCT0, SCT_EVENT_2_VAL, 
-        SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH) | 
         SCT_EV_CTRL_L_EVENT |
+        SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH) | 
         SCT_EV_CTRL_MATCHSEL(SCT_MATCH_2));
     SctSetEventStateMask(SCT0, SCT_EVENT_2_VAL, SCT_STATE_ALL_BIT);
     // TODO: Disable triac control output
 
-    // if we get a negative edge on the input, start the timer
-    // assign timer restart to event 3
+    // if we get a negative edge on the input, reset and start the timer
     SctSetEventControl(SCT0, SCT_EVENT_3_VAL, 
+        SCT_EV_CTRL_L_EVENT |
         SCT_EV_CTRL_INSEL | 
         SCT_EV_CTRL_IOSEL(0) | 
         SCT_EV_CTRL_IOCOND(SCT_IOCOND_FALL) |
         SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_IO) );
     SctSetEventStateMask(SCT0, SCT_EVENT_3_VAL, SCT_STATE_ALL_BIT);
-    // if we get a positive edge on the input, stop and clear the timer
-    // assign timer stop to event 4
+
+    // if we get a positive edge on the input, stop the timer
     SctSetEventControl(SCT0, SCT_EVENT_4_VAL, 
+        SCT_EV_CTRL_L_EVENT |
         SCT_EV_CTRL_INSEL | 
         SCT_EV_CTRL_IOSEL(0) | 
         SCT_EV_CTRL_IOCOND(SCT_IOCOND_RISE) |
         SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_IO) );
     SctSetEventStateMask(SCT0, SCT_EVENT_4_VAL, SCT_STATE_ALL_BIT);
 
+    // SCR pin control event
+    SctMatchL(SCT0, SCT_MATCH_0, 5000);
+    SctMatchReloadL(SCT0, SCT_MATCH_0, 5000);
+    SctSetEventControl(SCT0, SCT_EVENT_5_VAL, 
+        SCT_EV_CTRL_MATCHSEL(SCT_MATCH_0) |
+        SCT_EV_CTRL_L_EVENT |
+        SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH) );
+    SctSetEventStateMask(SCT0, SCT_EVENT_5_VAL, SCT_STATE_ALL_BIT);
+    SctOutput(SCT0, 0x00);
+
+    // assign conditions to events
     SctStartL(SCT0, SCT_EVENT_3_BIT);
     SctStopL(SCT0, SCT_EVENT_2_BIT | SCT_EVENT_4_BIT);
     SctLimitL(SCT0, SCT_EVENT_3_BIT);
+    SctOutputSet(SCT0, SCT_OUTPUT_1_VALUE, SCT_EVENT_5_BIT);
+    SctOutputClear(SCT0, SCT_OUTPUT_1_VALUE, SCT_EVENT_2_BIT | SCT_EVENT_4_BIT);
 
     setup50HzOutput();
     SctSetControl(SCT0, SCT_CTRL_STOP_L);
